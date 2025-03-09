158866eb26bc9719356a26dc3592d103ecf889a0
Dorian Kita <dorian.kita90@gmail.com>
2025-03-09 15:09:35 +0100

    created get_destination_data to store data from database

--- data_manager.py
+++ data_manager.py
@@ -1,2 +1,22 @@
+from dotenv import load_dotenv
+import os
+import requests
+from pprint import pprint
+from requests.auth import HTTPBasicAuth
+
+load_dotenv()
+SHEETY_ENDPOINT= 'https://api.sheety.co/de9862cff422df325716dc388558dc1d/flightDeals/prices'
+
 class DataManager:
-    pass
+
+    def __init__(self):
+        self._username = os.environ['SHEETY_USERNAME']
+        self._password = os.environ['SHEETY_PASSWORD']
+        self.authorization = HTTPBasicAuth(username=self._username, password=self._password)
+
+
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_ENDPOINT, auth=self.authorization)
+        sheet_data = response.json()['prices']
+        return sheet_data
+

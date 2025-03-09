9881a6a1110ad79d5909e94815cf19347451d352
Dorian Kita <dorian.kita90@gmail.com>
2025-03-09 16:28:17 +0100

    created function for getting access token to amadeus api

--- flight_search.py
+++ flight_search.py
@@ -1,5 +1,28 @@
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+TOKEN_ENDPOINT = 'https://test.api.amadeus.com/v1/security/oauth2/token'
+
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
+    def __init__(self):
+        self._api_key = os.environ['AM_API_KEY']
+        self._api_secret = os.environ['AM_API_SECRET']
+        self._token = self._get_new_token()
+
+    def _get_new_token(self):
+        header = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        body = {
+            'grant_type': 'client_credentials',
+            'client_id': self._api_key,
+            'client_secret': self._api_secret
+        }
+        response = requests.post(url=TOKEN_ENDPOINT, headers=header, data=body)
+        return response.json()['access_token']
 
     def get_destination_code(self, city_name):
         code = "TESTING"
